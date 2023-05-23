b4b0dc4e6e420fd91b8be3b07fc582f0c50f5c80
oskarlaak <oskarlaak@gmail.com>
2023-05-23 14:57:18 +0300

    default router setup

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,10 @@
-function App() {
-  return (
-    <>
-    Hello world!
-    </>
-  )
-}
+import { Routes, Route } from "react-router-dom";
+import Competitions from "./routes/Competitions";
 
-export default App
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Competitions/>}/>
+        </Routes>
+    );
+}
---
+++ front/src/routes/Competitions.tsx
@@ -0,0 +1,5 @@
+export default function Competitions() {
+    return <>
+        Competitions
+    </>;
+}
